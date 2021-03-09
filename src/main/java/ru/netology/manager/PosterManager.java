d1110e709bf64421249e632b7f0c7619f011811c
paulfltv <pavelfltv@yandex.ru>
2021-03-09 12:28:31 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -0,0 +1,38 @@
+package ru.netology.manager;
+
+import ru.netology.domain.MoviePoster;
+
+public class PosterManager {
+	private MoviePoster[] items = new MoviePoster[0];
+
+	public void add(MoviePoster item) {
+		int length = items.length + 1;
+		MoviePoster[] tmp = new MoviePoster[length];
+		System.arraycopy(items, 0, tmp, 0, items.length);
+		int lastIndex = tmp.length - 1;
+		tmp[lastIndex] = item;
+		items = tmp;
+	}
+
+	public MoviePoster[] getAll() {
+		MoviePoster[] result = new MoviePoster[items.length];
+		for (int i = 0; i < result.length; i++) {
+			int index = items.length - i - 1;
+			result[i] = items[index];
+		}
+		return result;
+	}
+
+	public void removeById(int id) {
+		int length = items.length - 1;
+		MoviePoster[] tmp = new MoviePoster[length];
+		int index = 0;
+		for (MoviePoster item : items) {
+			if (item.getId() != id) {
+				tmp[index] = item;
+				index++;
+			}
+		}
+		items = tmp;
+	}
+}
---
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -0,0 +1,49 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.MoviePoster;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class PosterManagerTest {
+	private PosterManager manager = new PosterManager();
+	private MoviePoster first = new MoviePoster(1, 1, "Bloodshot", "action", 16, true);
+	private MoviePoster second = new MoviePoster(2, 2, "Onward", "animated", 6, true);
+	private MoviePoster third = new MoviePoster(3, 3, "Hotel 'Belgrade'", "comedy", 6, true);
+	private MoviePoster fourth = new MoviePoster(4, 4, "The Gentlemen", "action", 18, true);
+	private MoviePoster fifth = new MoviePoster(5, 5, "The Invisible Man", "horror", 18, true);
+	private MoviePoster sixth = new MoviePoster(6, 6, "Trolls World Tour", "animated", 6, false);
+	private MoviePoster seventh = new MoviePoster(7, 7, "Number One", "comedy", 12, false);
+
+	@BeforeEach
+	public void setUp() {
+		manager.add(first);
+		manager.add(second);
+		manager.add(third);
+		manager.add(fourth);
+		manager.add(fifth);
+		manager.add(sixth);
+		manager.add(seventh);
+	}
+
+	@Test
+	public void shouldRemoveMovieIfExists() {
+		int idToRemove = 1;
+		manager.removeById(idToRemove);
+		MoviePoster[] actual = manager.getAll();
+		MoviePoster[] expected = new MoviePoster[]{seventh, sixth, fifth, fourth, third, second};
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	@Disabled
+	public void shouldNotRemoveIfNotExists() {
+		int idToRemove = 4;
+		manager.removeById(idToRemove);
+		MoviePoster[] actual = manager.getAll();
+		MoviePoster[] expected = new MoviePoster[]{seventh, sixth, fifth, fourth, third, second, first};
+		assertArrayEquals(expected, actual);
+	}
+}
